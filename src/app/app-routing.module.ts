e9310ae07547f04dede2e4e63669bf9d1025f821
Gus Prado <49629739+GusPrado@users.noreply.github.com>
2020-03-11 01:13:00 -0300

    Initial deployment

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router'
+
+import {HomeComponent} from '../app/home/home.component'
+import {ListComponent} from '../app/list/list.component'
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent},
+  { path: 'list', component: ListComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
